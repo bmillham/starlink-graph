a3fd7bc76f34fa5cb2691bd8daecc77debb7f9cd
Brian Millham <bmillham@gmail.com>
2022-08-13 02:43:56 -0400

    Completed switching to using the new module. More testing needed.

--- Signals/__init__.py
+++ Signals/__init__.py
@@ -7,6 +7,7 @@ import os
 import datetime
 from humanize import naturalsize
 #from humanize.time import naturaldelta
+import subprocess
 
 
 class Signals(object):
@@ -25,8 +26,14 @@ class Signals(object):
         self._configfile = configfile
         self._config = config
 
-    def set_sd(self, sd):
+    @property
+    def sd(self):
+        return self._sd
+
+    @sd.setter
+    def sd(self, sd):
         self._sd = sd
+
 
     @staticmethod
     def on_window1_destroy(widget):
@@ -91,3 +98,69 @@ class Signals(object):
     def on_create_animation_clicked(self, widget):
         task = self._on_create_animation_clicked()
         GLib.idle_add(task.__next__)
+
+    def _on_create_animation_clicked(self):
+        self._widgets['ani_button'].set_label('Creating')
+        self._widgets['ani_button'].set_sensitive(False)
+        self._widgets['ani_window'].show()
+        yield True
+        self._widgets['ani_progress'].pulse()
+        obs_dir = self._opts.get('obstructionhistorylocation')
+        if obs_dir == '':
+            return
+
+        video_format = self._widgets['video_format_cb'].get_model()[self._opts.getint('video_format')][1]
+        video_codec = self._widgets['video_codec_cb'].get_model()[self._opts.getint('video_codec')][1]
+        out_size = self._widgets['video_size_cb'].get_model()[self._opts.getint('video_size')][0]
+        out_dir = self._widgets['animation_output_directory'].get_filename()
+        duration = self._widgets['video_duration_spin_button'].get_value()
+        dir_list = os.listdir(obs_dir)
+        self._widgets['animation_directory_label'].set_text(obs_dir)
+
+        if len(dir_list) < duration:  # Make sure that there is at least enough images for a 1FPS video
+            self._widgets['ani_progress'].set_text('Not enough files to create animation')
+            self._widgets['ani_progress'].set_fraction(1.0)
+            self._widgets['ani_button'].set_sensitive(True)
+            self._widgets['ani_button'].set_label('Done')
+            yield False
+            return
+
+        frame_rate = len(dir_list) / duration
+        name_template = f'obstruction_animation_{datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.{video_format}'
+        self._widgets['animation_file_label'].set_text(name_template)
+        cat_pipe = subprocess.Popen(f"cat {obs_dir}/*.png", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+        ff_cmd = ["ffmpeg",
+                  "-f",
+                  "image2pipe",
+                  "-r",
+                  str(frame_rate),
+                  "-i",
+                  "-",
+                  "-vcodec",
+                  video_codec,
+                  "-s",
+                  out_size,
+                  "-pix_fmt",
+                  "yuv420p",
+                  f"{out_dir}/{name_template}"]
+
+        output = subprocess.Popen(ff_cmd, bufsize=1, universal_newlines=True, stdin=cat_pipe.stdout,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        self._widgets['ani_progress'].pulse()
+        yield True
+        while True:
+            line = output.stderr.readline()
+            if not line:
+                break
+            self._widgets['ani_progress'].set_text(line.rstrip())
+            self._widgets['ani_progress'].pulse()
+            yield True
+        cat_pipe.stdout.close()
+        cat_pipe.wait()
+        self._widgets['ani_progress'].set_text('Created')
+        self._widgets['ani_progress'].set_fraction(1.0)
+        self._widgets['ani_button'].set_label('Done')
+        self._widgets['ani_button'].set_sensitive(True)
+        yield False
--- starlink-graph.py
+++ starlink-graph.py
@@ -16,7 +16,6 @@ import sys
 import configparser
 import os
 import importlib
-import png
 import time
 import subprocess
 from Signals import Signals
@@ -25,117 +24,11 @@ from Signals import Signals
 # pip3 install humanize
 try:
     from humanize import naturalsize
-    from humanize.time import naturaldelta
 except ModuleNotFoundError:
     print('Humazine module not installed. Install with pip3')
-
-
     # Use a tacky simple naturalsize
     def naturalsize(x):
         return f"{x:.1f} kB"
-
-
-    def naturaldelta(x):
-        return x
-
-
-class Window1Signals:
-    def __init__(self):
-        self._obstructionstimer = None
-
-    def on_nogrpc_ok_button_clicked(self, widget):
-        pass
-
- #   def enable_map_cb(self, widget):
- #       save_map_when_window_closed_cb.set_sensitive(True)
-
-#    def clear_history_button_clicked(self, widget):
-#        obstructionhistorylocation.unselect_all()
-#        save_map_when_window_closed_cb.set_sensitive(False)
-
-    def save_history_cb_toggled(self, widget):
-        pass
-
-    def save_map_when_window_closed_cb_toggled(self, widget):
-        self.auto_obstruction_toggle()
-        #if save_map_when_window_closed_cb.get_active():
-    """ def on_create_animation_clicked(self, widget):
-        #ani_window.show()
-        #ani_out_buffer.set_text('Creating animation\n')
-        #self._on_create_animation_clicked()
-        pass
-
-    def on_create_animation_clicked(self, widget):
-        task = self._on_create_animation_clicked()
-        GLib.idle_add(task.__next__)
-
-    def _on_create_animation_clicked(self):
-        ani_button.set_label('Creating')
-        ani_button.set_sensitive(False)
-        ani_window.show()
-        yield True
-        ani_progress.pulse()
-        obs_dir = opts.get('obstructionhistorylocation')
-        if obs_dir == '':
-            return
-
-        video_format = video_format_cb.get_model()[opts.getint('video_format')][1]
-        video_codec = video_codec_cb.get_model()[opts.getint('video_codec')][1]
-        out_size = video_size_cb.get_model()[opts.getint('video_size')][0]
-        out_dir = animation_output_directory.get_filename()
-        duration = video_duration_spin_button.get_value()
-        dir_list = os.listdir(obs_dir)
-        animation_directory_label.set_text(obs_dir)
-
-        if len(dir_list) < duration: # Make sure that there is at least enough images for a 1FPS video
-            ani_progress.set_text('Not enough files to create animation')
-            ani_progress.set_fraction(1.0)
-            ani_button.set_sensitive(True)
-            ani_button.set_label('Done')
-            yield False
-            return
-
-        frame_rate = len(dir_list) / duration
-        name_template = f'obstruction_animation_{datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.{video_format}'
-        animation_file_label.set_text(name_template)
-        cat_pipe = subprocess.Popen(f"cat {obs_dir}/*.png", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-        ff_cmd = ["ffmpeg",
-                  "-f",
-                  "image2pipe",
-                  "-r",
-                  str(frame_rate),
-                  "-i",
-                  "-",
-                  "-vcodec",
-                  video_codec,
-                  "-s",
-                  out_size,
-                  "-pix_fmt",
-                  "yuv420p",
-                  f"{out_dir}/{name_template}"]
-
-        output = subprocess.Popen(ff_cmd, bufsize=1, universal_newlines=True, stdin=cat_pipe.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        ani_progress.pulse()
-        yield True
-#        while True:
-#            line = output.stderr.readline()
-#            if not line:
-#                break
-#            ani_progress.set_text(line.rstrip())
-#            ani_progress.pulse()
-#            yield True
-#        cat_pipe.stdout.close()
-#        cat_pipe.wait()
-#        ani_progress.set_text('Created')
-#        ani_progress.set_fraction(1.0)
-#        ani_button.set_label('Done')
-#        ani_button.set_sensitive(True)
-#        yield False"""
-
-#    def on_ani_window_delete(self, widget, event=None):
-#        ani_window.hide()
-#        return True
 
 config = configparser.ConfigParser()
 configfile = 'starlink-graph.ini'
@@ -364,8 +257,7 @@ if StarlinkData is None:
     widgets['nogrpswindow'].show_all()
 else:
     sd = StarlinkData(opts=opts)
-    my_signals.set_sd(sd)
-    #my_signals.sd = sd
+    my_signals.sd = sd
     ani = startup()
 
 widgets['window1'].show_all()
